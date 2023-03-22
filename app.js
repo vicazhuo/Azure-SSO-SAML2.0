aba0040bb612dca860bfc2814466963867d08611
731661902@qq.com <731661902@qq.com>
2023-03-22 11:38:02 +0800

    fix

--- app.js
+++ app.js
@@ -20,14 +20,12 @@ passport.deserializeUser(function (user, done) {
 passport.use(new SamlStrategy(
     {
         // path: '/login/callback',
-        entryPoint: 'https://login.microsoftonline.com/003eb209-7464-4ea7-a971-ae1d22b94ab4/saml2',
-        issuer: 'APM-Platform-SSO',
-        callbackUrl: 'https://platform.apmgroup.net/login/callback',
+        entryPoint: '',
+        issuer: '',
+        callbackUrl: '',
         // cert: fs.readFileSync('./SampleSAMLApp.cer', 'utf-8'),
-        cert: 'MIIC8DCCAdigAwIBAgIQOqfSt8cmioJOjCIEHhlWfzANBgkqhkiG9w0BAQsFADA0MTIwMAYDVQQDEylNaWNyb3NvZnQgQXp1cmUgRmVkZXJhdGVkIFNTTyBDZXJ0aWZpY2F0ZTAeFw0yMjEyMTkwOTU5NTBaFw0yNTEyMTkwOTU5NTBaMDQxMjAwBgNVBAMTKU1pY3Jvc29mdCBBenVyZSBGZWRlcmF0ZWQgU1NPIENlcnRpZmljYXRlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsP/CxZVLgMPbVGVtACj1YY+zPjT66N7m2c8Qf9Hv/ORAqF6tBDHbSq8zWd0A7Rd53ndFTIdwpC64pN1zrtlBox8v0FRwL2u4VQbauwKThYbw3DObGgzRFxmTJWz08WfztJMUX4kGFu4lyIN+pPfYYuJe9RcK8TKBe7cz5qkSYMNInnrsy28GgH5g4DnWm/K3OK+umjfHqxnjIIANoSl3NNRsbXfyvdch+qnoqxc1j8FeRkA9dqFG+kxsZT6C2Kw/3JWEBti2b0uVpj5Fs9WiMpgB5D8KsUU6o+8RL/6ha1wQz8OT1Sz/jWQhqptL9jFkzBTRkdZGe8gDcFyJD3QjKQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQA15cBLtNogWsKw5nj7p/oyBV03RXex9QYHlexwK/+2HDakUs1CeRNiytHo2/Oay+qzup2KO6ewe9buKRD0ASrHfuZBSN/YuEqgv6U+E6I66aeBZZCCVv2V9RA+Zs3ccuMS2+CxuepAhRW7NDAjM6RN5FkULB46nEWJnJen8bn9HJNd7ubZQfrUvjblkS3XE9E2Ws8Tu5gmcK7dGsO92XQpBYP0stRdz1F6PrGJ9vJAenUg4N4rE0T1he75u4L3RnhlJlXpMye0E87lvaiKlLZgVHSYLuVC4juiZcNbi/zg0BDZ0VFZTNykzYeGyQM94vD8sEBeSHA85wRAaKhg+gPK',
-        // authnContext: 'http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/windows',
+        cert: '',
         identifierFormat: null
-        // signatureAlgorithm: 'sha256'
     },
     function (profile, done) {
         return done(null,
